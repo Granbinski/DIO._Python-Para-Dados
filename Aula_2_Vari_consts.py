329c2517c6ff6d29784c329ccff239ad80aa5bfd
Inacio Grandini Ulbinski <inaciogranbinski@gmail.com>
2022-10-01 01:11:19 -0300

    update

--- Aula_2_Vari_consts.py
+++ Aula_2_Vari_consts.py
@@ -1,1 +1,8 @@
-nome = 
+nome = "Ignus"
+idade = "21"
+print(nome, idade)
+limite_saque_diario = 1000
+STATES = ["SP", "RJ"]
+BRAZILIAN_STATES = ["SP", "RJ"]
+nome, idade = "ignus", "22"
+print(nome, idade)
