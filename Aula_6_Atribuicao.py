5e0ea08644b4cd806a3689d6d73b6750a49e8a34
Inacio Grandini Ulbinski <inaciogranbinski@gmail.com>
2022-10-02 01:58:52 -0300

    update

---
+++ Aula_6_Atribuicao.py
@@ -0,0 +1,23 @@
+saldo = 500
+print(saldo)
+
+saldo += 200
+print(saldo)
+
+saldo -= 100
+print(saldo)
+
+saldo /= 2
+print(saldo)
+
+saldo //= 15
+print(saldo)
+
+saldo **= 10
+print(saldo)
+
+saldo *= 15
+print(saldo)
+
+saldo %= 15
+print(saldo)
