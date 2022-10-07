6e682cf5c3c46baf59424410de229c9b4e26cb9a
Inacio Grandini Ulbinski <inaciogranbinski@gmail.com>
2022-10-07 01:57:49 -0300

    update

--- Aula_10_Indentacao_Blocos.py
+++ Aula_10_Indentacao_Blocos.py
@@ -1,19 +1,14 @@
-saldo = 0
-valor = 0
-
-
 def sacar(valor):
     saldo = 500
-    if saldo == valor:
-        print("valor sacado")
+    if saldo >= valor:
+        print("Valor sacado")
+        print("Retire no caixa")
 
     print("Orbigado")
 
 
 def depositar(valor):
     saldo = 500
-
-
-saldo += valor
+    saldo += valor
 
 sacar(1000)
---
+++ Aula_11_Condicionais.py
@@ -0,0 +1,17 @@
+MAIOR_IDADE = 18
+IDADE_ESPECIAL = 12
+
+idade = int(input("Informe sua idade: "))
+
+if idade >= MAIOR_IDADE:
+    print("Pode tirar a CNH")
+
+if idade < MAIOR_IDADE:
+    print("Aida nao pode tirar CNH")
+    
+if idade >+ MAIOR_IDADE:
+    print("Pode tirar a CNH")
+elif idade == IDADE_ESPECIAL:
+    print("Pode fazer aula teorica apenas")
+else:
+    print("Nao pode tirar a CNH")
